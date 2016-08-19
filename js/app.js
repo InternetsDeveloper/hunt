7d2341b0f93fde50281880b0a1db691ebe1226e5
InternetsDeveloper <a.guy06@yahoo.com>
2016-08-19 10:50:22 -0500

    Initialize Start with Background

--- js/app.js
+++ js/app.js
@@ -5,4 +5,14 @@ $(document).ready(function () {
         url: 'sounds/music.mp3'
     }).play();
 
+    //Initialize Start
+    $('#main').animate({
+        opacity: 0
+    }, 0).css({
+        'background-image': 'url(\'../images/background.png\');'
+    }).animate({
+        opacity: 1
+    }, 2300);
+
+
 })
