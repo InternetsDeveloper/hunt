2e6fa08a28a693be86280daa2e738cad98f7975b
InternetsDeveloper <a.guy06@yahoo.com>
2016-08-20 21:11:31 -0500

    Set Level Parameters

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
 $(document).ready(function () {
 
-    //--==== Initialize Music ====--
-    soundManager.createSound({
-        url: 'sounds/music.mp3'
-    }).play();
+            //--==== Initialize Music ====--
+            soundManager.createSound({
+                url: 'sounds/music.mp3'
+            }).play();
 
-    //--==== Initialize Start ====--
-    $('#main').animate({
-        opacity: 0
-    }, 0).css({
-        'background-image': 'url(\'../images/background.png\');'
-    }).animate({
-        opacity: 1
-    }, 2300);
+            //--==== VAR sets ====--
 
-    //--====== End of Doc Ready ======--
-});
+            var lvlParams = [[3, 5],
+                            [4, 3],
+                            [3, 1]];
+
+            //--==== Initialize Start ====--
+            $('#main').animate({
+                opacity: 0
+            }, 0).css({
+                'background-image': 'url(\'../images/background.png\');'
+            }).animate({
+                opacity: 1
+            }, 2300);
+
+            timeoutSet(function () {
+                    wave(lvlParams[currentLevel][0], lvlParams[currentLevel[1]);
+                    }, 2300);
+
+                //--====== End of Doc Ready ======--
+            });
