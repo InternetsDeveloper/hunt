3078b1b8f8f19266d1fd05465accf60e379f122b
InternetsDeveloper <a.guy06@yahoo.com>
2016-08-31 15:51:36 -0500

    continue playing button / initialize play

--- js/app.js
+++ js/app.js
@@ -50,194 +50,194 @@ $(document).ready(function () {
     }, 2300);
 
     timeoutSet(function () {
-            wave(lvlParams[currentLevel][0], lvlParams[currentLevel[1]);
-            }, 2300);
+        wave(lvlParams[currentLevel][0], lvlParams[currentLevel][1]);
+    }, 2300);
 
-        //--==== Initialize Play Sound ====--
+    //--==== Initialize Play Sound ====--
 
-        function startPlay() {
-            soundManager.setup({
-                url: 'js/soundmanager/script/swf',
-                flashVersion: 9,
-                onready: function () {
-                    var gameSound = soundManager.createSound({
-                        'id': 'aSound',
-                        'multiShotEvents': true
-                    });
-                }
-            });
-        };
-
-        //--==== Kim Flight Patterns / Animation Seq ====--
-
-        function animateKim(newKim) {
-            var kimAnimate = (Math.floor(Math.random() * 10));
-            switch (kimAnimate) {
-
-                case 0:
-                    newKim.animate({
-                        "left": "85%",
-                        "top": "50%"
-                    }, 2000, "swing").delay(700).animate({
-                        "left": "50%",
-                        "top": "20%"
-                    }, 1000, "swing").delay(500).animate({
-                        "top": "50%",
-                        "left": "10%"
-                    }, 1250, "swing").delay(600).animate(flyKim1, 400, "swing");
-                    break;
-
-                case 1:
-                    newKim.animate({
-                        "left": "35%",
-                        "top": "30%"
-                    }, 1500, "swing").delay(650).animate({
-                        "left": "20%",
-                        "top": "35%"
-                    }, 950, "swing").delay(900).animate({
-                        "left": "70%",
-                        "top": "67%"
-                    }, 1000, "swing").delay(600).animate(flyKim1, 400, "swing");
-                    break;
-
-                case 2:
-                    newKim.animate({
-                        "left": "25%",
-                        "top": "60%"
-                    }, 1500, "swing").delay(500).animate({
-                        "left": "20%",
-                        "top": "35%"
-                    }, 800, "swing").delay(900).animate({
-                        "left": "70%",
-                        "top": "67%"
-                    }, 1000, "swing").delay(300).animate(flyKim1, 400, "swing");
-                    break;
-
-                case 3:
-                    newKim.animate({
-                        "left": "25%",
-                        "top": "40%"
-                    }, 1500, "swing").delay(700).animate({
-                        "left": "70%",
-                        "top": "27%"
-                    }, 1500, "swing").delay(500).animate({
-                        "top": "36%",
-                        "left": "10%"
-                    }, 1250, "swing").delay(600).animate({
-                        "left": "50%",
-                        "top": "15%"
-                    }, 1500, "swing").delay(750).animate(flyKim2, 400, "swing");
-                    break;
-
-                case 4:
-                    newKim.animate({
-                        "left": "0%",
-                        "top": "20%"
-                    }, 1500, "swing").delay(650).animate({
-                        "left": "60%",
-                        "top": "40%"
-                    }, 1450, "swing").delay(900).animate({
-                        "left": "60%",
-                        "top": "80%"
-                    }, 1000, "swing").delay(600).animate({
-                        "left": "80%",
-                        "top": "5%"
-                    }, 1000, "swing").delay(750).animate(flyKim2, 400, "swing");
-                    break;
-
-                case 5:
-                    newKim.animate({
-                        "left": "35%",
-                        "top": "60%"
-                    }, 1500, "swing").delay(500).animate({
-                        "left": "20%",
-                        "top": "35%"
-                    }, 800, "swing").delay(900).animate({
-                        "left": "10%",
-                        "top": "57%"
-                    }, 1000, "swing").delay(300).animate({
-                        "left": "20%",
-                        "top": "15%"
-                    }, 1500, "swing").delay(750).animate(flyKim2, 400, "swing");
-                    break;
-
-                case 6:
-                    newKim.animate({
-                        "left": "30%",
-                        "top": "10%"
-                    }, 1700, "swing").delay(1000).animate({
-                        "left": "30%",
-                        "top": "66%"
-                    }, 1000, "swing").delay(500).animate({
-                        "top": "63%",
-                        "left": "1%"
-                    }, 1250, "swing").delay(600).animate({
-                        "left": "29%",
-                        "top": "45%"
-                    }, 1500, "swing").delay(750).animate(flyKim2, 400, "swing");
-                    break;
-
-                case 7:
-                    newKim.animate({
-                        "left": "25%",
-                        "top": "33%"
-                    }, 1500, "swing").delay(700).animate({
-                        "left": "70%",
-                        "top": "27%"
-                    }, 1500, "swing").delay(500).animate({
-                        "top": "36%",
-                        "left": "10%"
-                    }, 1250, "swing").delay(600).animate({
-                        "left": "50%",
-                        "top": "15%"
-                    }, 1500, "swing").delay(750).animate(flyKim2, 400, "swing");
-                    break;
-
-                case 8:
-                    newKim.animate({
-                        "left": "0%",
-                        "top": "25%"
-                    }, 1500, "swing").delay(650).animate({
-                        "left": "60%",
-                        "top": "40%"
-                    }, 1450, "swing").delay(900).animate({
-                        "left": "60%",
-                        "top": "80%"
-                    }, 1000, "swing").delay(600).animate({
-                        "left": "80%",
-                        "top": "47%"
-                    }, 1000, "swing").delay(750).animate(flyKim2, 400, "swing");
-                    break;
-
-                case 9:
-                    newKim.animate({
-                        "left": "35%",
-                        "top": "80%"
-                    }, 1500, "swing").delay(500).animate({
-                        "left": "20%",
-                        "top": "35%"
-                    }, 800, "swing").delay(900).animate({
-                        "left": "15%",
-                        "top": "80%"
-                    }, 1500, "swing").delay(300).animate({
-                        "left": "60%",
-                        "top": "23%"
-                    }, 1000, "swing").delay(750).animate(flyKim2, 400, "swing");
-                    break;
-
-                    //-- end of Switch --
+    function startPlay() {
+        soundManager.setup({
+            url: 'js/soundmanager/script/swf',
+            flashVersion: 9,
+            onready: function () {
+                var gameSound = soundManager.createSound({
+                    'id': 'aSound',
+                    'multiShotEvents': true
+                });
             }
-            //--== end of animateKim func ==--
-        }
+        });
+    }
 
-    });
+    //--==== Kim Flight Patterns / Animation Seq ====--
+
+    function animateKim(newKim) {
+        var kimAnimate = (Math.floor(Math.random() * 10));
+        switch (kimAnimate) {
+
+            case 0:
+                newKim.animate({
+                    "left": "85%",
+                    "top": "50%"
+                }, 2000, "swing").delay(700).animate({
+                    "left": "50%",
+                    "top": "20%"
+                }, 1000, "swing").delay(500).animate({
+                    "top": "50%",
+                    "left": "10%"
+                }, 1250, "swing").delay(600).animate(flyKim1, 400, "swing");
+                break;
+
+            case 1:
+                newKim.animate({
+                    "left": "35%",
+                    "top": "30%"
+                }, 1500, "swing").delay(650).animate({
+                    "left": "20%",
+                    "top": "35%"
+                }, 950, "swing").delay(900).animate({
+                    "left": "70%",
+                    "top": "67%"
+                }, 1000, "swing").delay(600).animate(flyKim1, 400, "swing");
+                break;
+
+            case 2:
+                newKim.animate({
+                    "left": "25%",
+                    "top": "60%"
+                }, 1500, "swing").delay(500).animate({
+                    "left": "20%",
+                    "top": "35%"
+                }, 800, "swing").delay(900).animate({
+                    "left": "70%",
+                    "top": "67%"
+                }, 1000, "swing").delay(300).animate(flyKim1, 400, "swing");
+                break;
+
+            case 3:
+                newKim.animate({
+                    "left": "25%",
+                    "top": "40%"
+                }, 1500, "swing").delay(700).animate({
+                    "left": "70%",
+                    "top": "27%"
+                }, 1500, "swing").delay(500).animate({
+                    "top": "36%",
+                    "left": "10%"
+                }, 1250, "swing").delay(600).animate({
+                    "left": "50%",
+                    "top": "15%"
+                }, 1500, "swing").delay(750).animate(flyKim2, 400, "swing");
+                break;
+
+            case 4:
+                newKim.animate({
+                    "left": "0%",
+                    "top": "20%"
+                }, 1500, "swing").delay(650).animate({
+                    "left": "60%",
+                    "top": "40%"
+                }, 1450, "swing").delay(900).animate({
+                    "left": "60%",
+                    "top": "80%"
+                }, 1000, "swing").delay(600).animate({
+                    "left": "80%",
+                    "top": "5%"
+                }, 1000, "swing").delay(750).animate(flyKim2, 400, "swing");
+                break;
+
+            case 5:
+                newKim.animate({
+                    "left": "35%",
+                    "top": "60%"
+                }, 1500, "swing").delay(500).animate({
+                    "left": "20%",
+                    "top": "35%"
+                }, 800, "swing").delay(900).animate({
+                    "left": "10%",
+                    "top": "57%"
+                }, 1000, "swing").delay(300).animate({
+                    "left": "20%",
+                    "top": "15%"
+                }, 1500, "swing").delay(750).animate(flyKim2, 400, "swing");
+                break;
+
+            case 6:
+                newKim.animate({
+                    "left": "30%",
+                    "top": "10%"
+                }, 1700, "swing").delay(1000).animate({
+                    "left": "30%",
+                    "top": "66%"
+                }, 1000, "swing").delay(500).animate({
+                    "top": "63%",
+                    "left": "1%"
+                }, 1250, "swing").delay(600).animate({
+                    "left": "29%",
+                    "top": "45%"
+                }, 1500, "swing").delay(750).animate(flyKim2, 400, "swing");
+                break;
+
+            case 7:
+                newKim.animate({
+                    "left": "25%",
+                    "top": "33%"
+                }, 1500, "swing").delay(700).animate({
+                    "left": "70%",
+                    "top": "27%"
+                }, 1500, "swing").delay(500).animate({
+                    "top": "36%",
+                    "left": "10%"
+                }, 1250, "swing").delay(600).animate({
+                    "left": "50%",
+                    "top": "15%"
+                }, 1500, "swing").delay(750).animate(flyKim2, 400, "swing");
+                break;
+
+            case 8:
+                newKim.animate({
+                    "left": "0%",
+                    "top": "25%"
+                }, 1500, "swing").delay(650).animate({
+                    "left": "60%",
+                    "top": "40%"
+                }, 1450, "swing").delay(900).animate({
+                    "left": "60%",
+                    "top": "80%"
+                }, 1000, "swing").delay(600).animate({
+                    "left": "80%",
+                    "top": "47%"
+                }, 1000, "swing").delay(750).animate(flyKim2, 400, "swing");
+                break;
+
+            case 9:
+                newKim.animate({
+                    "left": "35%",
+                    "top": "80%"
+                }, 1500, "swing").delay(500).animate({
+                    "left": "20%",
+                    "top": "35%"
+                }, 800, "swing").delay(900).animate({
+                    "left": "15%",
+                    "top": "80%"
+                }, 1500, "swing").delay(300).animate({
+                    "left": "60%",
+                    "top": "23%"
+                }, 1000, "swing").delay(750).animate(flyKim2, 400, "swing");
+                break;
+
+                //-- end of Switch --
+        }
+        //--== end of animateKim func ==--
+    }
+
+});
 
 //--==== Level Complete Message / Options ====--
 
 function lvlComplete(numOfKims, bullets) {
     currLvl++;
 
-    console.log(currPlayer + " is the Current Shooter")
+    console.log(currPlayer + " is the Current Shooter");
     if (currLvl < lvlParams.length) {
         $("#main").html('<a class="button" id="lvlUp">FOR REALS! NEXT LEVEL!</a>');
         $("#main").append('<a class="button whatevs" id="whatevs">Whatevs..I\'m out</a>');
@@ -249,6 +249,37 @@ function lvlComplete(numOfKims, bullets) {
             setTimeout(function () {
                 wave(lvlParams[currLvl][0], lvlParams[currLvl][1]);
             }, 1000);
+        });
+
+        //--==== Quit Message / Button ====--
+        $("#main a#whatevs").on("click", function () {
+            $("#main").html('<h3 class="really">OMG! Like, really?</h3>');
+            $("#main").append('<a href="" class="button whatevs" id="cantEven">I can\'t even</a>');
+
+            //--==== Initialize Quit ====--
+            $("#main a#cantEven").on("click", function () {
+                console.log("Like, Start Next");
+                $('.shots').html("");
+                $('.kims').html("");
+                $("#main").html("");
+                setTimeout(function () {
+                    wave(lvlParams[currLvl][0], lvlParams[currLvl][1]);
+                }, 1000);
+            });
+
+            //--==== Play Button ====--
+            $("#main").append('<a class="button whatevs" id="wait">Omg! Like, hold on! I need to play</a>');
+
+            //--==== Continue Playing ====--
+            $("#content a#wait").on("click", function () {
+                console.log("Like, Start Next Level!");
+                $('.shots').html("");
+                $('.kims').html("");
+                $("#main").html("");
+                setTimeout(function () {
+                    wave(lvlParams[currLvl][0], lvlParams[currLvl][1]);
+                }, 1000);
+            });
         });
     }
 }
